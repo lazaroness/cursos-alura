21db4e930a4c5bac9e2d7fe3c9aa414b4a8a5b19
Lazarone Santos Santana <lazarone@objectdata.com.br>
2020-09-23 14:18:38 -0300

    aula-4

--- ruby-1-introducao-a-logica-de-programacao-com-jogos/maior_ou_menor.rb
+++ ruby-1-introducao-a-logica-de-programacao-com-jogos/maior_ou_menor.rb
@@ -4,3 +4,13 @@ puts "Qual é o seu nome?"
 nome = gets
 3.times do puts end
 puts "Começaremos o jogo pra você, " + nome
+3.times do puts end
+puts "Escolhendo um número secreto entre 0 e 200..."
+numero_secreto = 175
+3.times do puts end
+puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+4.times do puts end
+puts "Tentativa 1"
+puts "Entre com um número:"
+chute = gets
+puts "Será que acertou? Você chutou #{chute}"
